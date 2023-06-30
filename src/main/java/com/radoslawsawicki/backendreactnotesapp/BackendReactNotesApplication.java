854f475d651053ef05d3d883f51c60a63d1d186a
Radoslaw Sawicki <48261599+Sawikr@users.noreply.github.com>
2023-07-01 01:16:40 +0200

    Notes: first commit

---
+++ src/main/java/com/radoslawsawicki/backendreactnotesapp/BackendReactNotesApplication.java
@@ -0,0 +1,13 @@
+package com.radoslawsawicki.backendreactnotesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendReactNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendReactNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radoslawsawicki/backendreactnotesapp/BackendReactNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radoslawsawicki.backendreactnotesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendReactNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
