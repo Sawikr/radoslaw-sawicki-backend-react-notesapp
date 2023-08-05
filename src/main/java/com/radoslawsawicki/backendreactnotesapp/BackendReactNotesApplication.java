9c04c07967bffc856f88aa08f315190de1ca779e
Radoslaw Sawicki <48261599+Sawikr@users.noreply.github.com>
2023-08-06 00:01:07 +0200

    Notes: adding Swagger

--- src/main/java/com/radoslawsawicki/backendreactnotesapp/BackendReactNotesApplication.java
+++ src/main/java/com/radoslawsawicki/backendreactnotesapp/BackendReactNotesApplication.java
@@ -1,14 +1,23 @@
 package com.radoslawsawicki.backendreactnotesapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition
 @EnableScheduling
 @SpringBootApplication
-public class BackendReactNotesApplication {
+public class BackendReactNotesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendReactNotesApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BackendReactNotesApplication.class);
     }
 }
