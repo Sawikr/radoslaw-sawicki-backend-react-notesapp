aa5ee8c75d4428708c9d18cfecf2d2d5d620f4ee
Radoslaw Sawicki <48261599+Sawikr@users.noreply.github.com>
2023-08-19 20:57:06 +0200

    Notes: update security package

--- src/main/java/com/radoslawsawicki/backendreactnotesapp/security/controller/LoginController.java
+++ src/main/java/com/radoslawsawicki/backendreactnotesapp/security/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @GetMapping("/showMyLoginPages")
+    @GetMapping("/api/showMyLoginPages")
     public String showMyLoginPages() {
         return "fancy-login";
     }
