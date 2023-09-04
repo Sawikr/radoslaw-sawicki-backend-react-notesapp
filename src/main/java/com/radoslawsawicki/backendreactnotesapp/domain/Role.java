617c3bf1f65174e6f684a817eca5d4256976f8b7
Radoslaw Sawicki <48261599+Sawikr@users.noreply.github.com>
2023-09-04 21:03:38 +0200

    Notes: adding Role and User domains and adding ManyToMany relationships

---
+++ src/main/java/com/radoslawsawicki/backendreactnotesapp/domain/Role.java
@@ -0,0 +1,44 @@
+package com.radoslawsawicki.backendreactnotesapp.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="ROLES")
+public class Role {
+
+    private Long id;
+    private String name;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @NonNull
+    @Column(name = "ID", unique = true)
+    public Long getId() {
+        return id;
+    }
+
+    @NonNull
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
---
+++ src/main/java/com/radoslawsawicki/backendreactnotesapp/domain/User.java
@@ -0,0 +1,76 @@
+package com.radoslawsawicki.backendreactnotesapp.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="USERS")
+public class User {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private Set<Role> roles;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @NonNull
+    @Column(name = "ID", unique = true)
+    public Long getId() {
+        return id;
+    }
+
+    @NonNull
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    @NonNull
+    @Column(name = "USERNAME")
+    public String getUsername() {
+        return username;
+    }
+
+    @NonNull
+    @Column(name = "EMAIL")
+    public String getEmail() {
+        return email;
+    }
+
+    @NonNull
+    @Column(name = "PASSWORD")
+    public String getPassword() {
+        return password;
+    }
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "USERS_ROLES",
+        joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"),
+            inverseJoinColumns = @JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")
+    )
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(username, user.username) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, username, email, password);
+    }
+}
