59f20ebb5922188d144551efb6c8a82d47513cfc
Radoslaw Sawicki <48261599+Sawikr@users.noreply.github.com>
2025-02-26 21:59:13 +0100

    Notes: adding cors configurer

--- src/main/java/com/radoslawsawicki/backendreactnotesapp/security/config/SpringSecurityConfig.java
+++ src/main/java/com/radoslawsawicki/backendreactnotesapp/security/config/SpringSecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableMethodSecurity
@@ -58,5 +60,15 @@ public class SpringSecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
