3b7ba0adc6f9fc935256f94849ed1b26f5e1a80e
Radoslaw Sawicki <48261599+Sawikr@users.noreply.github.com>
2026-06-14 20:05:07 +0200

    Notes: updating application.properties to run Heroku

--- src/main/java/com/radoslawsawicki/backendreactnotesapp/security/config/SpringSecurityConfig.java
+++ src/main/java/com/radoslawsawicki/backendreactnotesapp/security/config/SpringSecurityConfig.java
@@ -19,8 +19,6 @@ import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
